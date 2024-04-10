7de733bc798819d5aedfd0890e437ba2e9f67ea4
Никита <immortallatrommi22@gmail.com>
2024-04-10 17:13:12 +0700

    build: fix cors problems 5

--- next.config.cjs
+++ next.config.cjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   pageExtensions: ["tsx", "mdx", "ts", "js"],
   experimental: {
     esmExternals: "loose",
